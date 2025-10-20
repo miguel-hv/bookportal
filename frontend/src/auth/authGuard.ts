aa10e30ad7b8957af12b86d5fa0d698b50da41f1
Miguel Huerta <huertadelavegamiguel@gmail.com>
2025-10-20 21:24:00 +0200

    fix:

--- frontend/src/auth/authGuard.ts
+++ frontend/src/auth/authGuard.ts
@@ -1,8 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-const REFRESH_URL = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/refresh`;
-
 export async function authGuard(req: NextRequest) {
   const accessToken = req.cookies.get("access_token")?.value;
   const refreshToken = req.cookies.get("refresh_token")?.value;
@@ -11,30 +9,6 @@ export async function authGuard(req: NextRequest) {
   if (!accessToken && !refreshToken) {
     url.pathname = "/login";
     return NextResponse.redirect(url);
-  }
-
- if (!accessToken && refreshToken) {
-    const refreshRes = await fetch(REFRESH_URL, {
-      method: "POST",
-      headers: { 
-        "Content-Type": "application/json",
-        Cookie: `refresh_token=${refreshToken}`
-      },
-      credentials: "include",
-    });
-
-    if (refreshRes.ok) {
-      const response = NextResponse.next();
-      const setCookies = refreshRes.headers.getSetCookie();
-      if (setCookies) {
-        for (const cookie of setCookies)
-          response.headers.append("Set-Cookie", cookie);
-      }
-      return response;
-    }
-     url.pathname = "/login";
-     console.log("redirect authguard")
-    // return NextResponse.redirect(url);
   }
 
   return NextResponse.next();
